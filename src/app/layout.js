ba299c4746dd419d593e4d6b5f22f73617ed5c8b
yusufhafizz <yusufshujauddin@gmail.com>
2026-03-18 16:18:32 +0530

    2

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,19 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "AVGC FramesShift",
   description: "A modern blog page",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "none",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
